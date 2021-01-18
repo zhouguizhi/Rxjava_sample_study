e890deb08eb523f736f9e48107ecb071d2a3d1b7
zhou.gz <2327322601@qq.com>
2021-01-18 14:44:08 +0800

    添加map变换操作符

--- app/src/main/java/com/zgz/rxjava/MainActivity.java
+++ app/src/main/java/com/zgz/rxjava/MainActivity.java
@@ -4,6 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.zgz.rxjava.util.LogUtil;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void filterOperator(View view) {
         startActivity(new Intent(this,FilterOperatorActivity.class));
+    }
+
+    public void transformOperator(View view) {
+        startActivity(new Intent(this,TransformOperatorActivity.class));
+    }
+
+    /**
+     * map变换操作符是把发送的数据经过中间变换后再发送到观察者
+     * @param view
+     */
+    public void onMapClickListener(View view) {
+        Observable.just(1).map(String::valueOf).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                LogUtil.e("onSubscribe:");
+            }
+            @Override
+            public void onNext(@NonNull String s) {
+                LogUtil.e("onNext:s>>>>>>>>"+s);
+            }
+            @Override
+            public void onError(@NonNull Throwable e) {
+                LogUtil.e("onError:");
+            }
+            @Override
+            public void onComplete() {
+                LogUtil.e("onComplete:");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/zgz/rxjava/TransformOperatorActivity.java
@@ -0,0 +1,17 @@
+package com.zgz.rxjava;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+/**
+ * @Description: 变换操作符
+ * @Author: zhouguizhi
+ * @CreateDate: 2021/1/18 下午2:13
+ * @Version: 1.0
+ */
+public class TransformOperatorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transform_operator);
+    }
+}
